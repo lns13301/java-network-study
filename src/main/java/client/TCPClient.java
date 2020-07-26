e3d572682dd82634662f815b040e8bc5c1b966fa
lns13301 <rkdrbgks22@ks.ac.kr>
2020-07-26 23:51:39 +0900

    feat: added client methods
    - TCP Client 클래스 생성
    - 서버 연결 메서드
    - 서버 연결 해제 메서드

---
+++ src/main/java/client/TCPClient.java
@@ -0,0 +1,34 @@
+package client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class TCPClient {
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 12345;
+    private Socket socket;
+
+    public TCPClient() {
+        socket = new Socket();
+    }
+
+    public void connectServer() {
+        try {
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnectServer() {
+        try {
+            if (socket != null && socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
